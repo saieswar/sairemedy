cfc3cdf84ee0fcceb5c70c1babfdda81b752b9e0
renukasalapu <salapurenuka@gmail.com>
2019-08-11 22:01:47 +0530

    Renuka - commented mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,15 +84,15 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.assets.debug = true
   config.action_mailer.default :charset=>"utf-8"
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address              => 'smtp.gmail.com',
-    :port                 => 587,
-    :domain               => 'gmail.com',
-    :user_name            => 'remedyvans@gmail.com',
-    :password             => 'pvnmurthii',
-    :authentication       => 'plain',
-    :enable_starttls_auto => true    
-  }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   :address              => 'smtp.gmail.com',
+  #   :port                 => 587,
+  #   :domain               => 'gmail.com',
+  #   :user_name            => 'remedyvans@gmail.com',
+  #   :password             => 'pvnmurthii',
+  #   :authentication       => 'plain',
+  #   :enable_starttls_auto => true    
+  # }
 
 end
